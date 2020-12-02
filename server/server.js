d6dd4cacf8b0be09c81cd218931485651fae96e1
Timothy Hansen <timothyhansen229@gmail.com>
2020-12-02 10:51:58 -0800

    CRUD operations complete for mongodb

--- server/server.js
+++ server/server.js
@@ -13,6 +13,40 @@ server.use(morgan('dev'));
 server.use(bodyParser.json());
 server.use(express.static(path.join(__dirname, '../client/dist')));
 
+
+// create a game
+server.post('/api/games', (req, res) => {
+  Game.create(req.body)
+  .then((results) => {
+    res.status(200).send(results)
+  })
+  .catch((err) => {
+    res.status(404).send(err)
+  })
+})
+
+// get all games
+server.get('/api/games', (req, res) => {
+  Game.find({})
+  .then((games) => {
+    res.status(200).send(games)
+  })
+  .catch((err) => {
+    res.status(400).send(err)
+  })
+})
+
+server.delete('/api/games/:id', (req, res) => {
+  Game.findOneAndDelete({_id: req.params.id})
+  .then(() => {
+    res.status(200).send('succesful delete')
+  })
+  .catch((err) => {
+    res.status(404).send(err)
+  })
+})
+
+// finds a random game in the list
 server.use('/api/games/one', (req, res) => {
   Game.find({})
     .then((games) => {
@@ -24,14 +58,18 @@ server.use('/api/games/one', (req, res) => {
     });
 });
 
+
+// finds 20 similar games
 server.use('/api/games/:id/similar', (req, res) => {
   Game.find({})
     .then((games) => {
       let similarGames = [];
 
       let gameIndex = games.findIndex((currGame) => {
+        // return first game whos id matches the request id
         return currGame._id.toString() === req.params.id.toString();
       })
+      // gets rid of the rest of the games
       games.splice(gameIndex, 1);
 
       for (let i = 0; i < 20; i++) {
@@ -46,6 +84,8 @@ server.use('/api/games/:id/similar', (req, res) => {
     });
 });
 
+
+//produces the 3 games freq bought together
 server.use('/api/games/:id/together', (req, res) => {
   Game.find({ _id: req.params.id })
     .then((game) => {
@@ -72,6 +112,16 @@ server.use('/api/games/:id/together', (req, res) => {
       res.send(err);
     });
 });
+
+server.put('/api/games/:id', (req, res) => {
+  Game.findOneAndUpdate({_id: req.params.id}, {$set: req.body})
+  .then((game) => {
+    res.status(200).send(game)
+  })
+  .catch((err) => {
+    res.status(404).send(err)
+  })
+})
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
